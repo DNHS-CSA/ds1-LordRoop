105e252fc768fc5d4b4a63d509c88489aea2dac9
Jagroop Vij <Jagroopvij@gmail.com>
2020-03-09 08:39:18 -0700

    Initial Commit

---
+++ SwiftUIProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftUIProject
+//
+//  Created by Vij, Jagroop on 3/9/20.
+//  Copyright © 2020 Vij, Jagroop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
